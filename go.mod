b6d3e4e83a2de479bc91e2fe44984274bece1a7a
赵思贵 <1060656096@qq.com>
2019-06-08 13:17:55 +0800

    hellomod v3版本提交

--- go.mod
+++ go.mod
@@ -1,1 +1,1 @@
-module github.com/qq1060656096/hellomod
+module github.com/qq1060656096/hellomod/v3
--- hello.go
+++ hello.go
@@ -1,5 +1,11 @@
 package hellomod
 
+package hellomod
+
 func Hello() string {
 	return "v2: Hello World!"
+}
+
+func HelloV3() string {
+	return "v3.HelloV3: Hello World!"
 }
